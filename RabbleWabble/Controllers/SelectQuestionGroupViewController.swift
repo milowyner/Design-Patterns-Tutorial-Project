069f90d3a9f324526e94e3a0b0841f8ed1676183
Milo Wyner <milowyner@gmail.com>
2020-11-23 13:58:45 -0800

    Finish chapter 4 on delegation

---
+++ RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SelectQuestionGroupViewController.swift
+//  RabbleWabble
+//
+//  Created by Milo Wyner on 11/19/20.
+//
+
+import UIKit
+
+public class SelectQuestionGroupViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet internal var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
+    
+    // MARK: - Properties
+    public let questionGroups = QuestionGroup.allGroups()
+    private var selectedQuestionGroup: QuestionGroup!
+}
+
+// MARK: - UITableViewDataSource
+extension SelectQuestionGroupViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionGroups.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell", for: indexPath) as! QuestionGroupCell
+        cell.titleLabel.text = questionGroups[indexPath.row].title
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SelectQuestionGroupViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        selectedQuestionGroup = questionGroups[indexPath.row]
+        return indexPath
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? QuestionViewController else {
+            print("Can't cast segue destination as \(QuestionViewController.self)")
+            return
+        }
+        viewController.questionGroup = selectedQuestionGroup
+        viewController.delegate = self
+    }
+}
+
+// MARK: - QuestionViewControllerDelegate
+extension SelectQuestionGroupViewController: QuestionViewControllerDelegate {
+    public func questionViewController(_ viewController: QuestionViewController, didCancel: QuestionGroup, at questionIndex: Int) {
+        navigationController?.popToViewController(self, animated: true)
+    }
+    
+    public func questionViewController(_ viewController: QuestionViewController, didComplete: QuestionGroup) {
+        navigationController?.popToViewController(self, animated: true)
+    }
+}
---
+++ RabbleWabble/Views/QuestionGroupCell.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroupCell.swift
+//  RabbleWabble
+//
+//  Created by Milo Wyner on 11/19/20.
+//
+
+import UIKit
+
+public class QuestionGroupCell: UITableViewCell {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var percentageLabel: UILabel!
+}
